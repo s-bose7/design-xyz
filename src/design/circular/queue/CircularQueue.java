8a6c53828e68152641c1bcf6041faf2e60a91ae5
Sudip Bose <sbose007ime.work@gmail.com>
2024-04-02 20:22:20 +0530

    circular queue implementation of queue interface uses a doubly-linked list as a backing data structure

--- src/design/circular/queue/CircularQueue.java
+++ src/design/circular/queue/CircularQueue.java
@@ -1,5 +1,92 @@
 package design.circular.queue;
 
-public class CircularQueue<E> implements Queue<E> {
+
+public class CircularQueue implements Queue {
+	
+	public ListNode front, rear;
+	public int size;
+	public final int MAX_SIZE;
+	
+	public CircularQueue(int n){
+		this.size = 0;
+		this.MAX_SIZE = n;
+		
+		this.front = this.rear = null;
+	}
+
+	@Override
+	public void addFirst(int element) {
+		// TODO Auto-generated method stub
+		if(isFull()){
+            return;
+        }
+		ListNode node = new ListNode(element);
+		if(front == null) {
+			front = rear = node;
+		}else {
+			rear.next = node;
+			node.prev = rear;
+			rear = node;
+		}
+		++size;
+	}
+
+	@Override
+	public int removeFirst() {
+		// TODO Auto-generated method stub
+		if(isEmpty()) {
+			return -1;
+		}
+		
+		ListNode node = front;
+		int val = node.val;
+		front = front.next;
+		node = null;
+		
+		return val;
+	}
+
+	@Override
+	public int getFirst() {
+		// TODO Auto-generated method stub
+		if(isEmpty()) {
+			return -1;
+		}
+		return front.val;
+	}
+
+	@Override
+	public int getLast() {
+		// TODO Auto-generated method stub
+		if(isEmpty()) {
+			return -1;
+		}
+		return rear.val;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return size == 0;
+	}
+
+	@Override
+	public boolean isFull() {
+		// TODO Auto-generated method stub
+		return size == MAX_SIZE;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		size = 0;
+		front = rear = null;
+	}
 
 }
