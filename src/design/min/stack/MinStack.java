e9c9dd645c774593dd8b075c2d8a70a8ea01e0cb
Sudip Bose <sbose007ime.work@gmail.com>
2024-04-08 16:50:50 +0530

    Implementation of MinStack

--- src/design/min/stack/MinStack.java
+++ src/design/min/stack/MinStack.java
@@ -1,5 +1,64 @@
 package design.min.stack;
 
 public class MinStack {
-
+	
+	private static class Node {
+		int value, minValue;
+		Node nextNode;
+		
+		public Node(int value) {
+			this.value = value;
+			this.minValue = Integer.MAX_VALUE;
+			this.nextNode = null;
+		}
+	}
+	
+	private Node peekNode;
+	private int minValue;
+	
+	public MinStack() {
+		this.peekNode = null;
+		this.minValue = Integer.MAX_VALUE;
+	}
+	
+	
+	public void push(int value) {
+		Node nodeToPush = new Node(value);
+		if(value < minValue) {
+			minValue = value;
+		}
+		nodeToPush.minValue = minValue;
+		if(peekNode == null) {
+			peekNode = nodeToPush;
+		}else {
+			nodeToPush.nextNode = peekNode;
+			peekNode = nodeToPush;
+		}
+	}
+	
+	public void pop() {
+		if(peekNode != null) {
+			Node topNode = peekNode;
+			peekNode = peekNode.nextNode;
+			topNode.nextNode = null;
+		}
+		
+		if (peekNode == null) {
+			minValue = Integer.MAX_VALUE;
+		}
+	}
+	
+	public int peek() {
+		if(peekNode == null) {
+			return -1;
+		}
+		return peekNode.value;
+	}
+	
+	public int getMin() {
+		if(peekNode == null) {
+			return -1;
+		}
+		return peekNode.minValue;
+	}
 }
