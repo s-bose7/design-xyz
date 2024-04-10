f63c33e18bf507a24990510c2090ca1296e93e47
Sudip Bose <sbose007ime.work@gmail.com>
2024-04-10 17:46:15 +0530

    test added BrowserHistory

--- test/browser/history/test/BrowserHistoryTest.java
+++ test/browser/history/test/BrowserHistoryTest.java
@@ -1,10 +1,29 @@
 package browser.history.test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import design.browser.histroy.BrowserHistory;
+
 public class BrowserHistoryTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@Test
+	public void testBrowserHistory() {
+		BrowserHistory browserHistory = new BrowserHistory("leetcode.com");
+		
+		browserHistory.visit("google.com");
+		browserHistory.visit("facebook.com");
+		browserHistory.visit("youtube.com");  
+		
+		assertEquals("facebook.com", browserHistory.back(1));
+		assertEquals("google.com", browserHistory.back(1));
+		assertEquals("facebook.com", browserHistory.forward(1));
+		browserHistory.visit("linkedin.com"); 
+		assertEquals("linkedin.com", browserHistory.forward(2));
+		assertEquals("google.com", browserHistory.back(2));
+		assertEquals("leetcode.com", browserHistory.back(7));
+		
 	}
 
 }
