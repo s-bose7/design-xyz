b7bffb93eccd3b15ae01abb653f9f629004ab3e5
Sudip Bose <sbose007ime.work@gmail.com>
2024-05-12 19:07:10 +0530

    added test skeleton

---
+++ test/binary/heap/test/BinaryHeapTest.java
@@ -0,0 +1,24 @@
+package binary.heap.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BinaryHeapTest {
+
+	@Test
+	public void binaryHeap() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void heapSort() {
+		
+	}
+	
+	@Test
+	public void priorityQueue() {
+		
+	}
+
+}
